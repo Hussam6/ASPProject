8dc066ec5cc9efa68288e5744c5ed9950a18f627
DeveloperPrabh <prabhsimran007@gmail.com>
2018-01-03 17:04:17 -0500

    All Changes are done in the model, had to drop old table, so made a new one, and will add it later. Next commint will be detailed

--- Project/Controllers/HomeController.cs
+++ Project/Controllers/HomeController.cs
@@ -3,26 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Project.Models;
 
 namespace Project.Controllers
 {
     public class HomeController : Controller
     {
+
+       
+
+        //Get
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
+        //    return View();
+        //}
 
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult Index(LoginController loginController)
         {
             ViewBag.Message = "Your contact page.";
+            
+
 
             return View();
         }
