953d2d840c3791af335437923ee089bfe1c27e48
Vincenzo <Vincenzo@DESKTOP-F1M262T>
2018-01-05 07:02:38 -0500

    Added Weather Controller Added API Model Added Latitude/Longitude Model Designed Weather Page

---
+++ Project/Models/WeatherModels/LatLngModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Project.Models.WeatherModels
+{
+    public class LatLngModel
+    {
+        [Required(ErrorMessage = "Please enter a Lattitude Value")]
+        public double lat { get; set; }
+
+        [Required(ErrorMessage = "Please enter a Longitude Value")]
+        public double lng { get; set; }
+    }
+}
